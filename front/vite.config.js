9a58cc7ebdf8278d1a92898f529bf8c1880eb8a6
ARAMOS <aldoramosaro@gmail.com>
2023-01-26 12:01:21 -0500

    cambios vite

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    watch: {
-      usePolling: true,
-    },
     host: true,
-    strictPort: true,
     port: 3000,
   }
 })
