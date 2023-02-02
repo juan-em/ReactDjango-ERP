42418a7b84a53f6ab4369d605e8d8f8cc5853062
ARamosA <aldoramosaro@gmail.com>
2023-02-02 13:09:05 -0500

    arreglado cloudinary

--- api/api_clientes/serializers.py
+++ api/api_clientes/serializers.py
@@ -1,21 +1,74 @@
 from rest_framework import serializers
 from drf_writable_nested.serializers import WritableNestedModelSerializer
 from django.contrib.auth.models import User
+from api_models.serializers import EmpresaSerializer, PersonaSerializer
 
 #Import models
 from api_models.models import (
-    Persona, Empresa, Clientes, Provincias, Formapago, Entidades
+    Clientes, Persona, Empresa
 )
 
-class PersonaSerializer (serializers.ModelSerializer):
+class ClienteSerializer(serializers.ModelSerializer):
+    persona = PersonaSerializer()
+    empresa = EmpresaSerializer()
     class Meta:
-        model = Persona
+        model = Clientes
         fields = '__all__'
+    def create(self, validated_data):
+        data_persona = validated_data.get("persona",None)
+        data_empresa = validated_data.get("empresa",None)
+        if data_persona:
+            empresa = None
+            data_persona = validated_data.pop("persona")
+            persona = Persona.objects.create(**data_persona)
+        if data_empresa:
+            persona = None
+            data_empresa = validated_data.pop("empresa")
+            empresa = Empresa.objects.create(**data_empresa)
+        proveedor = Clientes.objects.create(**validated_data,persona=persona,empresa=empresa)
+        return proveedor
+    
+    def update(self, instance,validated_data):
+        #Getting the values of the instance(the prov who equals the id)
+        persona = instance.persona
+        empresa = instance.empresa
+        #Updating persona
+        if persona:
+            data_persona = validated_data.pop("persona")
+            persona.nombre = data_persona.get("nombre",persona.nombre)
+            persona.dni = data_persona.get("dni",persona.dni)
+            persona.codprovincia = data_persona.get("codprovincia",persona.codprovincia)
+            persona.localidad = data_persona.get("localidad",persona.localidad)
+            persona.direccion = data_persona.get("direccion",persona.direccion)
+            persona.codpostal = data_persona.get("codpostal",persona.codpostal)
+            persona.cuentabancaria = data_persona.get("cuentabancaria",persona.cuentabancaria)
+            persona.telefono = data_persona.get("telefono",persona.telefono)
+            persona.movil = data_persona.get("movil",persona.movil)
+            persona.web = data_persona.get("web",persona.web)
+            persona.save()
+        #Updating empresa
+        if empresa:
+            data_empresa = validated_data.pop("empresa")
+            empresa.nombre = data_empresa.get("nombre",empresa.nombre)
+            empresa.estructurajuridica = data_empresa.get("estructurajuridica",empresa.estructurajuridica)
+            empresa.codprovincia = data_empresa.get("codprovincia",empresa.codprovincia)
+            empresa.tipo = data_empresa.get("tipo",empresa.tipo)
+            empresa.localidad = data_empresa.get("localidad",empresa.localidad)
+            empresa.direccion = data_empresa.get("direccion",empresa.direccion)
+            empresa.codpostal = data_empresa.get("codpostal",empresa.codpostal)
+            empresa.cuentabancaria = data_empresa.get("cuentabancaria",empresa.cuentabancaria)
+            empresa.telefono = data_empresa.get("telefono",empresa.telefono)
+            empresa.movil = data_empresa.get("movil",empresa.movil)
+            empresa.web = data_empresa.get("web",empresa.web)
+            empresa.save()
+        #Updating proveedor
+        instance.ruc = validated_data.get("ruc", instance.ruc)
+        instance.save()
+        return instance
+    
+    
 
-class EmpresaSerilizer(serializers.ModelSerializer):
-    class Meta:
-        model = Empresa
-        fields = '__all__'
+
 
 class ClientePersonaSerilizer(WritableNestedModelSerializer):
     persona = PersonaSerializer()
@@ -26,24 +79,10 @@ class ClientePersonaSerilizer(WritableNestedModelSerializer):
 
 
 class ClienteEmpresaSerilizer(WritableNestedModelSerializer):
-    empresa = EmpresaSerilizer()
+    empresa = EmpresaSerializer()
     class Meta:
         model = Clientes
         fields = ['id','empresa','codformapago','borrado'] 
  
 
-class ProvinciasSerilizer(serializers.ModelSerializer):
-    class Meta:
-        model = Provincias
-        fields = '__all__'
-
-class FormapagoSerilizer(serializers.ModelSerializer):
-    class Meta:
-        model = Formapago
-        fields = '__all__'
-
-
-class EntidadesSerilizer(serializers.ModelSerializer):
-    class Meta:
-        model = Entidades
-        fields = '__all__'
+        
