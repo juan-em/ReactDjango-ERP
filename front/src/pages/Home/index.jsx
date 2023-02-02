ea12e238538a8bc36b926f602c0a20f92d17c8c2
Anthonella Rua <anthonella.rua@tecsup.edu.pe>
2023-02-02 12:39:11 -0500

    home falta arreglar

--- front/src/pages/Home/index.jsx
+++ front/src/pages/Home/index.jsx
@@ -1,7 +1,53 @@
+import Menu from "../../components/Menu";
+import "./index.css";
+import ImageListItem from '@mui/material/ImageListItem';
+import Grid from '@mui/material/Grid';
 
 const Home = () =>{
+    const itemData = [
+        {
+          img: 'https://images.unsplash.com/photo-1589182337358-2cb63099350c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+          title: 'Breakfast',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1557015735-a762a259d9af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGxhbmElMjBhbHBhY2F8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+          title: 'Burger',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1552474705-dd8183e00901?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YWxwYWNhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+          title: 'Camera',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1616252993439-7e1924e5c29b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGFscGFjYSUyMGxhbmF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+          title: 'Coffee',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1636371449439-e19a1b5a25b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+          title: 'Honey',
+        },
+        {
+          img: 'https://images.unsplash.com/photo-1474904200416-6b2b7926f26f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+          title: 'Hats',
+        }
+    ];
     return(
-        <h1>INDEX</h1>
+        <section>
+            <Menu/>
+            <section class="container">
+                <Grid container spacing={1}>
+                    {itemData.map((item) => (
+                        <Grid item xs={4}>
+                            <ImageListItem key={item.img}>
+                            <img
+                                src={`${item.img}fit=crop&auto=format`}
+                                alt={item.title}
+                            />
+                            </ImageListItem>
+                        </Grid>
+                    ))}
+                </Grid>
+            </section>
+        </section>
     )
 }
 
