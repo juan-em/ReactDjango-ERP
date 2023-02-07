88354ac49b8b621e38de7fa087ca57d3a6e4b46b
ARamosA <aldoramosaro@gmail.com>
2023-02-07 08:55:56 -0500

    pagina principal

--- front/src/resources/menuItems.jsx
+++ front/src/resources/menuItems.jsx
@@ -1,4 +1,3 @@
-import MenuIcon from '@mui/icons-material/Menu'
 import HomeIcon from '@mui/icons-material/Home';
 import HandshakeIcon from '@mui/icons-material/Handshake';
 import Inventory2Icon from '@mui/icons-material/Inventory2';
@@ -8,41 +7,65 @@ import CategoryIcon from '@mui/icons-material/Category';
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
-import SearchIcon from '@mui/icons-material/Search';
+import SwitchAccountIcon from '@mui/icons-material/SwitchAccount';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import SellIcon from '@mui/icons-material/Sell';
+import ArticleIcon from '@mui/icons-material/Article';
+import ShopIcon from '@mui/icons-material/Shop';
+import SubjectIcon from '@mui/icons-material/Subject';
+import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
+import ElectricalServicesIcon from '@mui/icons-material/ElectricalServices';
+import WhereToVoteIcon from '@mui/icons-material/WhereToVote';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import LocalConvenienceStoreIcon from '@mui/icons-material/LocalConvenienceStore';
+import AddBusinessIcon from '@mui/icons-material/AddBusiness';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import RequestQuoteIcon from '@mui/icons-material/RequestQuote';
+import FolderCopyIcon from '@mui/icons-material/FolderCopy';
+import AlbumIcon from '@mui/icons-material/Album';
+import ArtTrackIcon from '@mui/icons-material/ArtTrack';
+
 
 export const menuItems = [
     { url: "/", icon: <HomeIcon/>, name: "Inicio",
     item:[] },
 
     { url: "#comerciales", icon: <HandshakeIcon/>, name: "Inter. Comer.",
-    item:[{name:'Cliente',icon: <HomeIcon/>,url:'#cliente'},
-        {name:'Proveedor',icon: <HomeIcon/>,url:'#proveedor'}] },
+    item:[{name:'Clientes',icon: <SwitchAccountIcon/>,url:'#cliente'},
+        {name:'Proveedores',icon: <SupportAgentIcon/>,url:'#proveedor'}] },
 
     { url: "#contact", icon: <Inventory2Icon/>, name: "Productos",
-    item:[{name:'Cliente',icon: <HomeIcon/>,url:'#cliente'},
-        {name:'Proveedor',icon: <HomeIcon/>,url:'#proveedor'}] },
+    item:[{name:'Productos',icon: <ArtTrackIcon/>,url:'#cliente'},
+        {name:'Articulos',icon: <AlbumIcon/>,url:'#proveedor'}] },
 
     { url: "#about", icon: <AttachMoneyIcon/>, name: "Ventas",
-    item:[{name:'Cliente',icon: <HomeIcon/>,url:'#cliente'},
-        {name:'Proveedor',icon: <HomeIcon/>,url:'#proveedor'}] },
+    item:[{name:'Venta',icon: <SellIcon/>,url:'#cliente'},
+        {name:'Facturas',icon: <ArticleIcon/>,url:'#proveedor'},
+        {name:'Remision',icon: <SubjectIcon/>,url:'#proveedor'}] },
 
     { url: "#about", icon: <ReceiptIcon/>, name: "Compras",
-    item:[{name:'Cliente',icon: <HomeIcon/>,url:'#cliente'},
-        {name:'Proveedor',icon: <HomeIcon/>,url:'#proveedor'}] },
+    item:[{name:'Compra',icon: <ShopIcon/>,url:'#cliente'},
+        {name:'Factura',icon: <ArticleIcon/>,url:'#proveedor'},
+        {name:'Remision',icon: <SubjectIcon/>,url:'#proveedor'}] },
 
     { url: "#about", icon: <CategoryIcon/>, name: "Producción",
-    item:[{name:'Cliente',icon: <HomeIcon/>,url:'#cliente'},
-        {name:'Proveedor',icon: <HomeIcon/>,url:'#proveedor'}] },
+    item:[{name:'Produccion',icon: <ProductionQuantityLimitsIcon/>,url:'#cliente'}] },
 
     { url: "#about", icon: <AutoStoriesIcon/>, name: "Tesorería",
-    item:[{name:'Cliente',icon: <HomeIcon/>,url:'#cliente'},
-        {name:'Proveedor',icon: <HomeIcon/>,url:'#proveedor'}] },
+    item:[{name:'Caja Diaria',icon: <PointOfSaleIcon/>,url:'#cliente'},
+        {name:'Libro Diario',icon: <BookmarkIcon/>,url:'#proveedor'}] },
 
     { url: "#about", icon: <DragIndicatorIcon/>, name: "Mantenimiento",
-    item:[{name:'Cliente',icon: <HomeIcon/>,url:'#cliente'},
-        {name:'Proveedor',icon: <HomeIcon/>,url:'#proveedor'}] },
+    item:[{name:'Provincias',icon: <WhereToVoteIcon/>,url:'#cliente'},
+        {name:'Formas de Pago',icon: <CreditCardIcon/>,url:'#proveedor'},
+        {name:'Entidades',icon: <LocalConvenienceStoreIcon/>,url:'#proveedor'},
+        {name:'Almacenes',icon: <AddBusinessIcon/>,url:'#proveedor'},
+        {name:'Impuestos',icon: <RequestQuoteIcon/>,url:'#proveedor'},
+        {name:'Embalajes',icon: <ArchiveIcon/>,url:'#proveedor'},
+        {name:'Categorias',icon: <FolderCopyIcon/>,url:'#proveedor'}] },
 
     { url: "#about", icon: <PrecisionManufacturingIcon/>, name: "Servicios",
-    item:[{name:'Cliente',icon: <HomeIcon/>,url:'#cliente'},
-        {name:'Proveedor',icon: <HomeIcon/>,url:'#proveedor'}] },
+    item:[{name:'Servicios',icon: <ElectricalServicesIcon/>,url:'#cliente'}] },
 ];
