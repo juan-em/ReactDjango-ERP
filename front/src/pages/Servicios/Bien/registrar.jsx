c01842c244a133a5fa3a9c6256a78c73b4422b91
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2023-04-23 16:23:13 -0500

    servicio/bienes: añadir un fieldarray

--- front/src/pages/Servicios/Bien/registrar.jsx
+++ front/src/pages/Servicios/Bien/registrar.jsx
@@ -1,6 +1,6 @@
 import './index.css';
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import {
   TextField,
@@ -14,13 +14,17 @@ import {
   Tab, Tabs, Box,
   Autocomplete, Card,
   Select, MenuItem,
-  FormControl, InputLabel
+  FormControl, InputLabel,
+  Divider
 } from "@mui/material";
+
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import RemoveCircleOutline from '@mui/icons-material/RemoveCircleOutline';
 
 import { TabContext } from "@mui/lab";
 
 // Imports relacionado con Formik
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, FieldArray } from 'formik';
 
 // Imports relacionado a los iconos
 import CloseIcon from "@mui/icons-material/Close";
@@ -28,12 +32,13 @@ import CloseIcon from "@mui/icons-material/Close";
 import PropTypes from 'prop-types';
 
 // Imports relacionado a las peticiones
-import { postBienes, transformToFormData } from '../../../services/Servicios/bienes';
+import { cotizacionInitialState, postBienes, transformToFormData } from '../../../services/Servicios/bienes';
+import { getProveedores } from '../../../services/Proveedores';
 
 const Registar = () => {
   // POST request para la orden de bien
   const ordenBienMenorSubmit = async (data) => {
-    var payload = transformToFormData(data)
+    //var payload = transformToFormData(data)
     console.log(data,"<===============")
     var testingBienes = {
       "bien_nombre":"bien1",
@@ -53,12 +58,15 @@ const Registar = () => {
       "bien_cotizacion":12.30
     }
 
-    var res = await postBienes(payload)
-    console.log("====================> ", res)
+    // var res = await postBienes(payload)
+    // console.log("====================> ", res)
   }
 
   // Funcionamiento relacion a la interfaz de registro con Tabs
   const [open, setOpen] = useState(false);
+  const [proveedores, setProveedores] = useState()
+  const [item, setItem] = useState(cotizacionInitialState)
+
   
   const handleOpen = () => {
     setOpen(true);
@@ -109,17 +117,7 @@ const Registar = () => {
     setValue(newValue);
   };
 
-  //para autocomplete
-  const handlerSearcher = (e) => {
-    const { name, value } = e.target;
-    setFields({ ...fields, [name]: value });
-  };
-  const top100Films = [
-    { label: 'Logistica'},
-    { label: 'Marketing'},
-    { label: 'Aministración'},
-    { label: 'Mantenimiento'},
-  ];
+
 
   const top101Films = [
     'Ninguno',
@@ -130,7 +128,11 @@ const Registar = () => {
 
   ];
 
-  const item = {}
+  useEffect(() => {
+    getProveedores(setProveedores)
+  }, [])
+  
+  
 
   return (
     <div>
@@ -317,8 +319,8 @@ const Registar = () => {
                 <TabPanel value={value} index={1}>
 
                   {/* Registro de bien en el que la cotizacion es mayor a 500 */}
-                  <Formik initialValues={item} >
-                      {({ values, handleSubmit, handleChange }) => (
+                  <Formik initialValues={item} onSubmit={ordenBienMenorSubmit}>
+                      {({ values, handleSubmit, handleChange, setFieldValue, setValues }) => (
                       <Form onSubmit={handleSubmit}>
                         <Grid container spacing={1}>
                           <Grid item xs={12} sm={6} md={6}>
@@ -360,134 +362,130 @@ const Registar = () => {
                             </Select>
                           </FormControl>
                           </Grid>
+
+                          
+                          
                           <Grid item xs={12} sm={12} md={12}>
-                            <Card variant="outlined" sx={{px:2, py:1}}>
-                              <Grid container spacing={1}>
-                                <Grid item xs={12} sm={12} md={6} >
-                                  <Grid container spacing={1}>
-                                    <Grid item xs={12} sm={12} md={12} >
-                                      <span>Cotización 1</span>
-                                    </Grid>
-                                    <Grid item xs={12} sm={12} md={12}>
-                                      {/* <Button variant="outlined" component="label" fullWidth size="small">
-                                        <span>Subir</span>
-                                        <input hidden accept="image/*" multiple type="file" />
-                                      </Button> */}
-                                      <Field name="file" variant="outlined" size="small" component="label">
-                                        {({ field, form }) => (
-                                          <TextField
-                                            type="file"
-                                            {...field}
-                                            inputProps={{
-                                              accept: '.pdf, .doc, .docx, .xls, .xlsx, application/pdf, application/msword, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                            <FieldArray
+                              name="empresas"
+                              render={(arrayHelpers) => (
+                                <>
+                                  <Card variant="outlined" sx={{px:2, py:1, border: "1px solid purple"}}>
+                                  {values.empresas.map((variante, index) => (
+                                      <div  key={index}>
+                                      <Grid container spacing={1}>
+                                        <Grid item xs={12} sm={12} md={12} >
+                                          <Autocomplete
+                                            fullWidth
+                                            type="text"
+                                            size="small"
+                                            color="secondary"
+                                            margin="dense"
+                                            id="textfields"
+                                            disablePortal
+                                            required
+                                            options={proveedores}
+                                            getOptionLabel = {(option) => {
+                                              if (option.persona) return option.persona.nombre 
+                                              if (option.empresa) return option.empresa.nombre
+                                              return ''
                                             }}
-                                            onBlur={() => form.setTouched({ file: true })}
-                                            onChange={(event) => setFieldValue('file', event.currentTarget.files[0])}
-                                            error={form.touched.file && form.errors.file}
-                                            helperText={form.touched.file && form.errors.file}
+                                            renderInput={(params) => 
+                                              <TextField 
+                                                {...params} 
+                                                label="Proveedor" 
+                                                margin="dense" 
+                                                color="secondary"
+                                                fullWidth />}
+                                            value={values.proveedor}
+                                            onChange={(e, value) => {setFieldValue(`empresas.${index}.proveedor`, value.id)}}
                                           />
-                                        )}
-                                      </Field>
-                                    </Grid>
-                                  </Grid>
-                                </Grid>
+                                        </Grid>
+                                        <Grid item xs={12} sm={12} md={6} >
+                                          <Grid container spacing={1}>
+                                            <Grid item xs={12} sm={12} md={12} >
+                                              <span>Cotización {index + 1}</span>
+                                            </Grid>
+                                            <Grid item xs={12} sm={12} md={12}>
+                                              {/* <Button variant="outlined" component="label" fullWidth size="small">
+                                                <span>Subir</span>
+                                                <input hidden accept="image/*" multiple type="file" />
+                                              </Button> */}
+                                              <Field name="file" variant="outlined" size="small" component="label">
+                                                {({ field, form }) => (
+                                                  <TextField
+                                                    type="file"
+                                                    {...field}
+                                                    inputProps={{
+                                                      accept: '.pdf, .doc, .docx, .xls, .xlsx, application/pdf, application/msword, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                                                    }}
+                                                    onBlur={() => form.setTouched({ file: true })}
+                                                    onChange={(event) => setFieldValue(`empresas.${index}.cotizacion.bien_archivo_cotizacion`, event.currentTarget.files[0])}
+                                                    error={form.touched.file && form.errors.file}
+                                                    helperText={form.touched.file && form.errors.file}
+                                                  />
+                                                )}
+                                              </Field>
+                                            </Grid>
+                                          </Grid>
+                                        </Grid>
 
-                                <Grid item xs={12} sm={12} md={6} >
-                                  <Grid container spacing={1}>
-                                    <Grid item xs={12} sm={12} md={12} >
-                                      <span>Propuesta económica 1</span>
-                                    </Grid>
-                                    <Grid item xs={12} sm={12} md={12}>
-                                    <Field name="file" variant="outlined" size="small" component="label">
-                                      {({ field, form }) => (
-                                        <TextField
-                                          type="file"
-                                          {...field}
-                                          inputProps={{
-                                            accept: '.pdf, .doc, .docx, .xls, .xlsx, application/pdf, application/msword, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                                          }}
-                                          onBlur={() => form.setTouched({ file: true })}
-                                          onChange={(event) => setFieldValue('file', event.currentTarget.files[0])}
-                                          error={form.touched.file && form.errors.file}
-                                          helperText={form.touched.file && form.errors.file}
-                                        />
-                                      )}
-                                    </Field>
-                                    </Grid>
-                                  </Grid>
-                                </Grid>
-                              </Grid>
-                            </Card>
+                                        <Grid item xs={12} sm={12} md={6} >
+                                          <Grid container spacing={1}>
+                                            <Grid item xs={12} sm={12} md={12} >
+                                              <span>Propuesta económica {index + 1}</span>
+                                            </Grid>
+                                            <Grid item xs={12} sm={12} md={12}>
+                                            <Field name="file" variant="outlined" size="small" component="label">
+                                              {({ field, form }) => (
+                                                <TextField
+                                                  type="file"
+                                                  {...field}
+                                                  inputProps={{
+                                                    accept: '.pdf, .doc, .docx, .xls, .xlsx, application/pdf, application/msword, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                                                  }}
+                                                  onBlur={() => form.setTouched({ file: true })}
+                                                  onChange={(event) => setFieldValue(`empresas.${index}.propuesta_economica.propuesta_tecnica_archivo`, event.currentTarget.files[0])}
+                                                  error={form.touched.file && form.errors.file}
+                                                  helperText={form.touched.file && form.errors.file}
+                                                />
+                                              )}
+                                            </Field>
+                                            </Grid>
+                                          </Grid>
+                                        </Grid>
+                                      </Grid>
+                                      {index != 0 &&
+                                      <Button
+                                        fullWidth
+                                        component="label"
+                                        variant="outlined"
+                                        startIcon={<RemoveCircleOutline/>}
+                                        sx={{ marginTop: "0.5rem" }}
+                                        onClick={() => arrayHelpers.remove(index)}
+                                      >
+                                        Eliminar
+                                      </Button>}
+                                      <Divider style={{  marginTop: "0.5rem", border: "1px dashed purple"}} />
+                                      </div>
+                                  ))}
+                                    <Button
+                                        fullWidth
+                                        component="label"
+                                        variant="outlined"
+                                        color="secondary"
+                                        startIcon={<AddCircleIcon />}
+                                        sx={{ marginTop: "0.5rem" }}
+                                        onClick={() => arrayHelpers.push({})}
+                                      >
+                                        Cotización
+                                    </Button>
+                                  </Card>
+                                </>
+                              )}
+                            />
                           </Grid>
-
-                          <Grid item xs={12} sm={12} md={12}>
-                            <Card variant="outlined" sx={{px:2, py:1}}>
-                              <Grid container spacing={1}>
-                                <Grid item xs={12} sm={12} md={6} >
-                                  <Grid container spacing={1}>
-                                    <Grid item xs={12} sm={12} md={12} >
-                                      <span>Cotización 2</span>
-                                    </Grid>
-                                    <Grid item xs={12} sm={12} md={12}>
-                                      <Button variant="outlined" component="label" fullWidth size="small">
-                                        <span>Subir</span>
-                                        <input hidden accept="image/*" multiple type="file" />
-                                      </Button>
-                                    </Grid>
-                                  </Grid>
-                                </Grid>
-
-                                <Grid item xs={12} sm={12} md={6} >
-                                  <Grid container spacing={1}>
-                                    <Grid item xs={12} sm={12} md={12} >
-                                      <span>Propuesta económica 2</span>
-                                    </Grid>
-                                    <Grid item xs={12} sm={12} md={12}>
-                                      <Button variant="outlined" component="label" fullWidth size="small">
-                                        <span>Subir</span>
-                                        <input hidden accept="image/*" multiple type="file" />
-                                      </Button>
-                                    </Grid>
-                                  </Grid>
-                                </Grid>
-                              </Grid>
-                            </Card>
-                          </Grid>
-
-                          <Grid item xs={12} sm={12} md={12}>
-                            <Card variant="outlined" sx={{px:2, py:1}}>
-                              <Grid container spacing={1}>
-                                <Grid item xs={12} sm={12} md={6} >
-                                  <Grid container spacing={1}>
-                                    <Grid item xs={12} sm={12} md={12} >
-                                      <span>Cotización 3</span>
-                                    </Grid>
-                                    <Grid item xs={12} sm={12} md={12}>
-                                      <Button variant="outlined" component="label" fullWidth size="small">
-                                        <span>Subir</span>
-                                        <input hidden accept="image/*" multiple type="file" />
-                                      </Button>
-                                    </Grid>
-                                  </Grid>
-                                </Grid>
-
-                                <Grid item xs={12} sm={12} md={6} >
-                                  <Grid container spacing={1}>
-                                    <Grid item xs={12} sm={12} md={12} >
-                                      <span>Propuesta económica 3</span>
-                                    </Grid>
-                                    <Grid item xs={12} sm={12} md={12}>
-                                      <Button variant="outlined" component="label" fullWidth size="small">
-                                        <span>Subir</span>
-                                        <input hidden accept="image/*" multiple type="file" />
-                                      </Button>
-                                    </Grid>
-                                  </Grid>
-                                </Grid>
-                              </Grid>
-                            </Card>
-                          </Grid>
-
+                          
                           <Grid item xs={12} sm={6} md={6} sx={{ mt: 4 }}>
                             <Button
                               fullWidth
--- front/src/services/Servicios/bienes.jsx
+++ front/src/services/Servicios/bienes.jsx
@@ -2,16 +2,17 @@ import axios from "../../api/axios";
 
 const BIENES_URL = "api/ordenes/bien/"
 
+export const cotizacionInitialState = {empresas:[{}]}
+
 export function transformToFormData (values) {
     const formData = new FormData();
     formData.append('bien_nombre', values.bien_nombre);
     formData.append('bien_estado', values.bien_estado);
-    formData.append('bien_cotizacion_archivo', values.bien_cotizacion_archivo);
-
-    values.orden_bien_tecnico.forEach((file, index) => {
-    formData.append(`orden_bien_tecnico[${index}][propuesta_tecnica_archivo]`, file.propuesta_tecnica_archivo);
+    
+    values.empresas.forEach((value, index) => {
+        formData.append(`empresas[${index}][propuesta_tecnica_archivo]`, value.proveedor);
+        formData.append(`orden_bien_tecnico[${index}][propuesta_tecnica_archivo]`, value.propuesta_tecnica_archivo);
     });
-
     values.orden_bien_economico.forEach((file, index) => {
     formData.append(`orden_bien_economico[${index}][propuesta_bien_archivo]`, file.propuesta_bien_archivo);
     });
