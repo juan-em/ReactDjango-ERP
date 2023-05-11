e844a46a536488e66296ca2f96b0cb20afdd8edc
Robert Charca <trevoraobac@gmail.com>
2023-05-11 11:44:07 -0500

    api_ordenes_bienes_servicios: campo de filtrado agregado

--- api/api_ordenes_bienes_servicios/serializers.py
+++ api/api_ordenes_bienes_servicios/serializers.py
@@ -45,4 +45,4 @@ class OrdenBienSerializer(WritableNestedModelSerializer):
 
     class Meta:
         model = Orden_bien
-        fields = ['id', 'bien_nombre', 'bien_estado', 'orden_bien','codigo']
+        fields = ['id', 'bien_nombre', 'bien_estado', 'orden_bien', 'mayor_500','codigo']
