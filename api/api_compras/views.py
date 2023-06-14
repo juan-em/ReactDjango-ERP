480b78c3265a2161dbae61b1b4958243d1af8a64
Robert Charca <trevoraobac@gmail.com>
2023-06-13 21:06:27 -0500

    nuevas migraciones y se configuro el timezone al horario peruano

--- api/api_compras/views.py
+++ api/api_compras/views.py
@@ -23,22 +23,22 @@ class ComprasView(APIView):
     def post(self, request):
 
         #registro caja
-        tipo_pago = request.data.pop('tipo_pago', None)
+        # tipo_pago = request.data.pop('tipo_pago', None)
         
         serializer = CompraSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
         #registro caja
-        if tipo_pago:
-            data = {
-                'tipo_movimiento': 'Compra',
-                'compra': Compra.objects.get(id=serializer.data["id"]),
-                'tipo_pago': Formapago.objects.get(id=tipo_pago),
-                'caja_diaria': Caja_diaria.objects.get(estado=True)
-            }
-            cajaMovimiento = Caja_diaria_movimientos.objects.create(**data)
-            cajaMovimiento.save()
+        # if tipo_pago:
+        #     data = {
+        #         'tipo_movimiento': 'Compra',
+        #         'compra': Compra.objects.get(id=serializer.data["id"]),
+        #         'tipo_pago': Formapago.objects.get(id=tipo_pago),
+        #         'caja_diaria': Caja_diaria.objects.get(estado=True)
+        #     }
+        #     cajaMovimiento = Caja_diaria_movimientos.objects.create(**data)
+        #     cajaMovimiento.save()
 
         context = {
                 'data':'OK',
