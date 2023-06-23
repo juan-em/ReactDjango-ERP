14074fb1c111ee1826f0a23ac4a0fc2fd67fa1a0
J-E-M A-R-N <Juan-E-M>
2023-06-22 23:11:17 -0500

    abrir y cerrar caja con resposable, ingresoss y egresos

--- front/src/pages/Tesorería/CajaDiaria/index.jsx
+++ front/src/pages/Tesorería/CajaDiaria/index.jsx
@@ -1,3 +1,4 @@
+import "./index.css";
 import { alpha } from "@mui/material/styles";
 import { useEffect, useState } from "react";
 
@@ -6,16 +7,17 @@ import {
   Grid,
   TextField,
   Button,
-  Box, Card,
+  Box,
+  Card,
   Typography,
-  Container
+  Container,
 } from "@mui/material";
 
 import Registro from "./registro";
-import dayjs from 'dayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import dayjs from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 import { Tabla } from "./complements";
 
 import UserRequest from "../../../components/User/Requests/UserRequest";
@@ -23,27 +25,44 @@ import { getLastCaja, patchCaja, postCaja } from "../../../services/caja";
 
 const CajaDiaria = () => {
   //para el input de fecha
-  const user = UserRequest()
-  
+  const user = UserRequest();
+  console.log(user)
+
   const [value, setValue] = useState(dayjs(new Date()));
-  const [itemCaja, setItemCaja] = useState({'estado_caja': false})
+  const [itemCaja, setItemCaja] = useState({ estado_caja: false });
 
   const handleChange = (newValue) => {
     setValue(newValue);
   };
 
   useEffect(() => {
-    getLastCaja(setItemCaja)
-  }, [])
+    getLastCaja(setItemCaja);
+  }, []);
 
-  const handleOpenCloseCaja = async() => {
-    itemCaja.estado_caja === true ? await patchCaja(itemCaja.id, {'estado_caja': false}) 
-                                  : await postCaja({'estado_caja': true}) 
-  }
+  const handleOpenCloseCaja = async () => {
+    if (itemCaja.estado_caja) {
+      await patchCaja(itemCaja.id, { estado_caja: false, responsable_cierre: user.trabajador.id});
+    } else {
+      var res = await postCaja({ estado_caja: true , responsable_apertura: user.trabajador.id});
+      setItemCaja(res.content);
+    }
+  };
+
+  var fechaHoraActual = new Date();
+  var fechaHoy = fechaHoraActual.toLocaleDateString();
+  var horaHoy = fechaHoraActual.toLocaleTimeString();
+
+  const formatFecha = (fechaOriginal) => {
+    var partesFecha = fechaOriginal.split("-");
+    var dia = partesFecha[2];
+    var mes = partesFecha[1];
+    var anio = partesFecha[0];
+    return dia + "/" + mes + "/" + anio;
+  };
 
   return (
     <Container>
-      <div className="container" >
+      <div className="container">
         <Typography
           fontFamily={"inherit"}
           align={"center"}
@@ -58,32 +77,34 @@ const CajaDiaria = () => {
         >
           Caja Diaria
         </Typography>
-        <Grid container spacing={4} sx={{ marginTop: '10px'}}>
+        <Grid container spacing={4} sx={{ marginTop: "10px" }}>
           <Grid item xs={12} sm={12} md={6} xl={6}>
-            <Card elevation={10} sx={{ p:5}}>
+            <Card elevation={10} sx={{ p: 5 }}>
               <Grid container spacing={1}>
                 <Grid item xs={12} sm={12} md={12} xl={12}>
-                  <Typography
-                    fontFamily={"inherit"}
-                    align={"center"}>
-                      Caja actual
-                    </Typography>
-                  </Grid>
+                  <Typography fontFamily={"inherit"} align={"center"}>
+                    Caja actual
+                  </Typography>
+                </Grid>
                 <Grid item xs={12} sm={12} md={6} xl={6}>
-                  <Card variant="outlined" sx={{p:1}}>
-                    <Typography
-                    fontFamily={"inherit"}
-                    align={"center"}>
-                      <strong>Fecha:</strong> 21/03/2023
+                  <Card variant="outlined" sx={{ p: 1 }}>
+                    <Typography fontFamily={"inherit"} align={"center"}>
+                      <strong>Fecha:</strong>{" "}
+                      {itemCaja.fecha_apertura
+                        ? formatFecha(itemCaja.fecha_apertura)
+                        : fechaHoy}
+                      ,{" "}
+                      {itemCaja.hora_apertura
+                        ? itemCaja.hora_apertura.slice(0, 5)
+                        : horaHoy}
                     </Typography>
                   </Card>
                 </Grid>
                 <Grid item xs={12} sm={12} md={6} xl={6}>
-                  <Card variant="outlined" sx={{p:1}}>
-                    <Typography
-                      fontFamily={"inherit"}
-                      align={"center"}>
-                      <strong>Saldo Inicial:</strong> S/. 10000
+                  <Card variant="outlined" sx={{ p: 1 }}>
+                    <Typography fontFamily={"inherit"} align={"center"}>
+                      <strong>Saldo Inicial:</strong> S/.{" "}
+                      {itemCaja.monto_inicial ? itemCaja.monto_inicial : "0"}
                     </Typography>
                   </Card>
                 </Grid>
@@ -91,17 +112,15 @@ const CajaDiaria = () => {
             </Card>
           </Grid>
           <Grid item xs={12} sm={12} md={6} xl={6}>
-            <Card elevation={10} sx={{ p:5}}>
+            <Card elevation={10} sx={{ p: 5 }}>
               <Grid container spacing={1}>
                 <Grid item xs={12} sm={12} md={12} xl={12}>
-                  <Typography
-                    fontFamily={"inherit"}
-                    align={"center"}>
-                      Acciones
-                    </Typography>
-                  </Grid>
+                  <Typography fontFamily={"inherit"} align={"center"}>
+                    Acciones
+                  </Typography>
+                </Grid>
                 <Grid item xs={12} sm={12} md={6} xl={6}>
-                  <Registro/>
+                  <Registro itemCaja={itemCaja}/>
                 </Grid>
                 <Grid item xs={12} sm={12} md={6} xl={6}>
                   <Button
@@ -109,10 +128,12 @@ const CajaDiaria = () => {
                     id="textfields"
                     color="secondary"
                     variant="contained"
-                    sx={{ m:1}}
+                    sx={{ m: 1 }}
                     onClick={handleOpenCloseCaja}
                   >
-                    {itemCaja.estado_caja == true ? "Cerrar Caja" : "Abrir Caja"}
+                    {itemCaja.estado_caja == true
+                      ? "Cerrar Caja"
+                      : "Abrir Caja"}
                   </Button>
                 </Grid>
               </Grid>
@@ -124,32 +145,35 @@ const CajaDiaria = () => {
               elevation={10}
               className="paper"
               sx={{
-              py: 2,
-              px: 5
+                py: 2,
+                px: 5,
               }}
             >
               <Typography
                 fontFamily={"inherit"}
-                align={"center"} sx={{ mb:2 }}>
-                  Buscar registro
-                </Typography>
+                align={"center"}
+                sx={{ mb: 2 }}
+              >
+                Buscar registro
+              </Typography>
               <Grid container spacing={1}>
                 <Grid item xs={12} sm={12} md={6} xl={6}>
                   <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DesktopDatePicker
-                    label="Fecha de apertura"
-                    inputFormat="DD/MM/YYYY"
-                    value={value}
-                    onChange={handleChange}
-                    renderInput={(params) => <TextField 
-                      {...params} 
-                      fullWidth
-                      size="small"
-                      color="secondary"
-                      id="textfields"
-                      margin="dense"
-                      
-                      />}
+                      label="Fecha de apertura"
+                      inputFormat="DD/MM/YYYY"
+                      value={value}
+                      onChange={handleChange}
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          fullWidth
+                          size="small"
+                          color="secondary"
+                          id="textfields"
+                          margin="dense"
+                        />
+                      )}
                     />
                   </LocalizationProvider>
                 </Grid>
@@ -194,9 +218,11 @@ const CajaDiaria = () => {
 
           <Grid item xs={12} sm={12} md={12} xl={12}>
             <Box sx={{ overflow: "auto" }}>
-              <Box sx={{ width: "100%", display: "table", tableLayout: "fixed" }}>
-                <Tabla/>
-            </Box>
+              <Box
+                sx={{ width: "100%", display: "table", tableLayout: "fixed" }}
+              >
+                <Tabla />
+              </Box>
             </Box>
           </Grid>
         </Grid>
--- front/src/services/caja.jsx
+++ front/src/services/caja.jsx
@@ -1,39 +1,95 @@
 import axios from "../api/axios";
 
-const cajaURL = "api/cajadiaria/"
-const lastCajaURL =  cajaURL + "ultimacaja/"
+const cajaURL = "api/cajadiaria/";
+const lastCajaURL = cajaURL + "ultimacaja/";
+const ingresosOtrosURL = cajaURL + "ingresosotros/";
+const egresosOtrosURL = cajaURL + "egresosotros/";
 
 export const postCaja = async (data) => {
-    try {
-        const response = await axios.post(cajaURL, data)
-        return response.data
-    } catch (err) {
-        console.log(err)
-        return err
-    }
-}
+  try {
+    const response = await axios.post(cajaURL, data);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
 
 export const getLastCaja = async (set) => {
-    try {
-        const responseLast = await axios.get(lastCajaURL)
-        
-        if (responseLast.data.status !== false) {
-            set(responseLast.data.content)
-        }
-        
-        return responseLast.data
-    } catch (err) {
-        console.log(err)
-        return err
+  try {
+    const responseLast = await axios.get(lastCajaURL);
+
+    if (responseLast.data.status !== false) {
+      set(responseLast.data.content);
     }
-}
+
+    return responseLast.data;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
 
 export const patchCaja = async (id, data) => {
+  try {
+    const responsePatch = await axios.patch(cajaURL + `${id}/`, data);
+    return responsePatch.data;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
+
+//registros de caja
+export const postIngresoCaja = async (data) => {
     try {
-        const responsePatch = await axios.patch(cajaURL + `${id}/`, data)
-        return responsePatch.data
-    } catch (err) {
-        console.log(err)
-        return err
-    } 
+        const res = await axios.post(ingresosOtrosURL, data);
+        return res.data;
+      } catch (err) {
+        console.log(err);
+        return err;
+      }
 }
+
+export const postEgresoCaja = async (data) => {
+    try {
+        const res = await axios.post(egresosOtrosURL, data);
+        return res.data;
+      } catch (err) {
+        console.log(err);
+        return err;
+      }
+}
+
+
+export const initialRegister = {
+  tipo: "Otros ingresos",
+  monto: 0.0,
+  tipo_pago: "Soles",
+  descripcion: "-",
+};
+
+export function transformToFormData(values) {
+  const formData = new FormData();
+  for (let key in values) {
+    if (values.hasOwnProperty(key)) {
+      const value = values[key];
+      if (value instanceof File) {
+        formData.append(key, value, value.name);
+      } else {
+        formData.append(key, value);
+      }
+    }
+  }
+  return formData;
+}
+
+//api sunat - cambio dolar
+export const getCambio = async (set) => {
+  axios
+    .get("http://localhost:8000/api/cajadiaria/cambio-dolar")
+    .then((res) => {
+      if (res.status == 200) set(res.data.tipo_cambio);
+    })
+    .catch((error) => console.log(error));
+};
